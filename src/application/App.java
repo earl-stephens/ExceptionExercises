f4ec3f319179fe30adcb1ff8e3595d4d4a3af4f3
earl-stephens <stephensearl@hotmail.com>
2022-02-24 23:28:43 -0700

    Move output message to try block

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int value = 0;
 		
+		System.out.println("Enter a number > ");
+		String line = scanner.nextLine();
+		
+		try {
+			value = Integer.parseInt(line);
+			/* if parseInt throws an exception, the program immediately
+			 * jumps to the catch block and doesn't execute anything 
+			 * below in the try block
+			 */
+			System.out.println("You entered: " + value);
+		}
+		catch (NumberFormatException e){
+			System.out.println("That is an invalid input.");
+		}
+		
+		scanner.close();
 	}
 }
